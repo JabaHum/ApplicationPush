6bda6c8312db5ef3edf2947efaa407683ab66a81
Jabar Jeremy <jababjnan8@gmail.com>
2019-08-28 22:48:27 +0300

    Edited EzyAgric Notification more

--- app/src/main/java/com/example/applicationpush/EzyAgricNotificationManager.java
+++ app/src/main/java/com/example/applicationpush/EzyAgricNotificationManager.java
@@ -2,8 +2,10 @@ package com.example.applicationpush;
 
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 
@@ -21,8 +23,9 @@ public class EzyAgricNotificationManager {
 
     public static synchronized EzyAgricNotificationManager getInstance(Context context){
         if (mInstance == null){
-            mInstance = new EzyAgricNotificationManager(context);
 
+            //context = context.getApplicationContext();
+            mInstance = new EzyAgricNotificationManager(context);
         }
         return mInstance;
 
@@ -30,19 +33,35 @@ public class EzyAgricNotificationManager {
 
     public  void displayNotification(String title ,String message){
 
+        Intent resultIntent  =  new Intent(mCtx,PushDetailsActivity.class);
+
+        // Set the Activity to start in a new, empty task
+        //resultIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        // Create the TaskStackBuilder and add the intent, which inflates the back stack
+        TaskStackBuilder stackBuilder = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            stackBuilder = TaskStackBuilder.create(mCtx);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            stackBuilder.addNextIntentWithParentStack(resultIntent);
+        }
+
+        // Get the PendingIntent containing the entire back stack
+        PendingIntent pendingIntent = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        }
+
 
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(mCtx,Constants.CHANNEL_ID);
         mBuilder.setSmallIcon(R.drawable.ic_block_black_24dp);
         mBuilder.setContentTitle(title);
         mBuilder.setContentText(message);
-
-        Intent resultIntent  =  new Intent(mCtx,PushDetailsActivity.class);
-        // Set the Activity to start in a new, empty task
-        resultIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(mCtx,0,resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
+        // Set the intent that will fire when the user taps the notification
         mBuilder.setContentIntent(pendingIntent);
+        mBuilder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        mBuilder.setAutoCancel(true);
 
         NotificationManager manager = (NotificationManager)mCtx.getSystemService(Context.NOTIFICATION_SERVICE);
 
@@ -50,8 +69,6 @@ public class EzyAgricNotificationManager {
         if (manager != null){
             manager.notify(0,mBuilder.build());
         }
-
-
 
     }
 
--- app/src/main/java/com/example/applicationpush/PushDetailsActivity.java
+++ app/src/main/java/com/example/applicationpush/PushDetailsActivity.java
@@ -2,6 +2,10 @@ package com.example.applicationpush;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 
 public class PushDetailsActivity extends AppCompatActivity {
@@ -10,5 +14,12 @@ public class PushDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_push_details);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
     }
 }
